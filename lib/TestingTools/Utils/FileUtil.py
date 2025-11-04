679c726d5d7afc20bbcf0723311e22ed66dfcc9b
KATPC\kkjee <kkjeer@wellesley.edu>
2025-11-03 18:18:24 -0600

    file util: add read file methods

--- lib/TestingTools/Utils/FileUtil.py
+++ lib/TestingTools/Utils/FileUtil.py
@@ -15,6 +15,37 @@ class FileUtil:
     self.shared_folder = config['scratch']
     logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
                         level=logging.INFO)
+    
+  # This method reads a workspace file using its file ref.
+  # This is the preferred way to read files.
+  def readFileById(self, ctx, file_ref):
+    if file_ref is None or file_ref == '':
+      logging.error('cannot read empty file ref')
+      return None
+    try:
+      ws = Workspace(self.ws_url, token=ctx['token'])
+      obj = ws.get_objects2({'objects' : [{'ref' : file_ref}]})
+      logging.info(f'read file {file_ref}: {obj}')
+      return obj
+    except Exception as e:
+      logging.error(f'could not read file {file_ref}: {e}')
+      return None
+    
+  # This method reads a workspace file using its file name (rather than its ref).
+  # This is a slower way to read a file (compared to using its ref), and should only be used if the ref is not available.
+  # (See https://github.com/kbaseapps/SpeciesTreeBuilder/blob/dce166f6d1673018a001b750c191b9a2deda0c71/lib/src/workspace/ObjectSpecification.java).
+  def readFileByName(self, ctx, file_name, workspace_name):
+    if file_name is None or file_name == '':
+      logging.error('cannot read empty file name')
+      return None
+    try:
+      ws = Workspace(self.ws_url, token=ctx['token'])
+      obj = ws.get_objects2({'objects' : [{'name' : file_name, 'find_reference_path': 1, 'workspace': workspace_name}]})
+      logging.info(f'read file {file_name}: {obj}')
+      return obj
+    except Exception as e:
+      logging.error(f'could not read file {file_name}: {e}')
+      return None
 
   # This is a helper method for writing a file to the workspace.
   def writeFile(self, data, name, file_type, description):
@@ -36,18 +67,20 @@ class FileUtil:
       version = save_result[0][4]
       workspace_id = save_result[0][6]
       ref = f'{workspace_id}/{id}/{version}'
-      return {'ref': ref, 'description': description}
+      return {'ref': ref, 'description': description or 'summary of results'}
     except Exception as e:
       logging.error(f'failed to save file {name} of type {file_type}: {e}')
       return None
 
   # Each of these methods is a wrapper for writing a particular file type to the workspace.
-  def writeStringTable(self, table_data):
-    return self.writeFile(table_data, 'string-data-table', 'MAK.StringDataTable', 'string data table')
+  def writeStringTable(self, table_data, file_name, description):
+    name = file_name or 'string-data-table'
+    return self.writeFile(table_data, name, 'MAK.StringDataTable', description)
     
-  def writeSampleSet(self, sample_set_data):
-    return self.writeFile(sample_set_data, 'sample-set', 'KBaseSets.SampleSet', 'sample set summary')
+  def writeSampleSet(self, sample_set_data, file_name, description):
+    name = file_name or 'sample-set'
+    return self.writeFile(sample_set_data, name, 'KBaseSets.SampleSet', description)
     
-  def writeAttributeMappingFile(self, mapping_data, file_name):
+  def writeAttributeMappingFile(self, mapping_data, file_name, description):
     name = file_name or 'attribute-mapping'
-    return self.writeFile(mapping_data, name, 'KBaseExperiments.AttributeMapping', 'summary of results')
+    return self.writeFile(mapping_data, name, 'KBaseExperiments.AttributeMapping', description)
