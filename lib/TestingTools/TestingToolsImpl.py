ca30d3d28c0c244c97ed3b1b1387188fd25146ef
KATPC\kkjee <kkjeer@wellesley.edu>
2026-03-22 13:48:02 -0500

    move cobrapy experiment into correct run function

--- lib/TestingTools/TestingToolsImpl.py
+++ lib/TestingTools/TestingToolsImpl.py
@@ -226,6 +226,42 @@ This sample module contains one small method that filters contigs.
         # Print statements to stdout/stderr are captured and available as the App log
         logging.info('Starting run_FBAExperiments function. Params=' + pformat(params))
 
+        test_cobrapy_tasks = [
+           {
+            'module_name': 'COBRApyBasedFBA',
+            'function_name': 'run_fba_pipeline',
+            'version': 'beta',
+            'parameters': {
+              'fba_output_id': 'cobrapy-test-output',
+              # 'target_reaction': 'bio1',
+              'fbamodel_id': params['fbamodel_id'],
+              'media_id': params['media_id'],
+              'workspace': params['workspace_name']
+            }
+          }
+        ]
+        logging.info(f'cobrapy tasks: {test_cobrapy_tasks}')
+        appexplorer_util = AppExplorerUtil(self.config)
+        test_cobrapy_result = appexplorer_util.runKBParallel(test_cobrapy_tasks)
+        logging.info(f'cobrapy result: {test_cobrapy_result}')
+
+        # Build the report
+        summary = f'tried to run cobrapy fba app'
+        reportObj = {
+          'objects_created': [],
+          'text_message': summary
+        }
+        report = KBaseReport(self.callback_url)
+        report_info = report.create({'report': reportObj, 'workspace_name': params['workspace_name']})
+
+        # Construct output
+        output = {'report_name': report_info['name'],
+                  'report_ref': report_info['ref']
+                  }
+
+        # return the results
+        return [output]
+
         app_explorer_util = AppExplorerUtil(self.config)
         file_util = FileUtil(self.config, ctx, params)
         output_util = OutputUtil(self.config)
@@ -346,42 +382,6 @@ This sample module contains one small method that filters contigs.
 
         # Print statements to stdout/stderr are captured and available as the App log
         logging.info('Starting run_FBABehaviorFeedback function. Params=' + pformat(params))
-
-        test_cobrapy_tasks = [
-           {
-            'module_name': 'COBRApyBasedFBA',
-            'function_name': 'run_fba_pipeline',
-            'version': 'beta',
-            'parameters': {
-              'fba_output_id': 'cobrapy-test-output',
-              # 'target_reaction': 'bio1',
-              'fbamodel_id': params['fbamodel_id'],
-              'media_id': params['media_id'],
-              'workspace': params['workspace_name']
-            }
-          }
-        ]
-        logging.info(f'cobrapy tasks: {test_cobrapy_tasks}')
-        appexplorer_util = AppExplorerUtil(self.config)
-        test_cobrapy_result = appexplorer_util.runKBParallel(test_cobrapy_tasks)
-        logging.info(f'cobrapy result: {test_cobrapy_result}')
-
-        # Build the report
-        summary = f'tried to run cobrapy fba app'
-        reportObj = {
-          'objects_created': [],
-          'text_message': summary
-        }
-        report = KBaseReport(self.callback_url)
-        report_info = report.create({'report': reportObj, 'workspace_name': params['workspace_name']})
-
-        # Construct output
-        output = {'report_name': report_info['name'],
-                  'report_ref': report_info['ref']
-                  }
-
-        # return the results
-        return [output]
 
         # Create utilities
         metamorphic_feedback_util = FBABehaviorFeedbackUtil(self.config)
